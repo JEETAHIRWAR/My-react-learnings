916ab900ad0b66c6cadffb0db2c6002c78ddc356
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-04-26 22:49:07 +0530

    Counter using useState in 02counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,35 +1,42 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
-  let value = 0
+let [value, setValue] = useState(0)
+
+
+  // let value = 0
 
   const increaseValue = () => {
     value = value+1
+    setValue(value)
     console.log(value)
 
-    document.querySelector('h2').textContent = `Value: ${value}`
+    // document.querySelector('h2').textContent = `Value: ${value}`
 
   }
 
   const decreaseValue = () => {
     value = value-1
+    setValue(value)
     console.log(value)
 
-    document.querySelector('h2').textContent = `Value: ${value}`
+    // document.querySelector('h2').textContent = `Value: ${value}`
   }
 
   const resetValue = () => {
     value = 0
+    setValue(value)
 
-    document.querySelector('h2').textContent = `Value: ${value}`
+    // document.querySelector('h2').textContent = `Value: ${value}`
   }
 
   return (
     <>
     <h1>Number Counter</h1>
 
-    <h2>Value: 0</h2>
+    <h2>Value: {value}</h2>
 
     <div>
     <button onClick={increaseValue}>Increase value</button>
