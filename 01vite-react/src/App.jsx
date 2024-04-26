63a3b9131c2009a9ad7301d8a7448b7e7411b517
JEET AHIRWAR <jeetahirwar664@gmail.com>
2024-04-26 20:59:09 +0530

    create new methods and variable use

--- 01vite-react/src/App.jsx
+++ 01vite-react/src/App.jsx
@@ -2,9 +2,10 @@ import Hello from "./Hello"
 
 
 function App() {
+  const name = "Jeet Ahirwar"
   return (
     <>
-      <h1>Hello World | React | Jeet</h1>
+      <h1>Hello World | React | {name}</h1>
       <Hello/>
     </>
   )
--- 01vite-react/src/main.jsx
+++ 01vite-react/src/main.jsx
@@ -2,8 +2,47 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+// function MyApp() {
+//   return (
+//     <>
+//       <h1>This Is A custom App!!</h1>
+//     </>
+//   )
+// }
+
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit Google'
+// }
+
+// const anotherElement = (
+//   <a href='https://google.com' target='_blank'>
+//     Click me to visit Google</a>
+// )
+
+// const reactElement = React.createElement(
+//   'a',
+//   {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   'Click me to visit Google'
+// )
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
+  // ReactElement
+
+  // anotherElement
+
+  // reactElement
 )
